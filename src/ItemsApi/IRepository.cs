4164a730cbf4975a2aff5030c4e07cb65964f536
karmerk <karmerk@gmail.com>
2022-05-17 20:58:08 +0200

    Repository
    To avoid having to fill out CRUD methods

--- src/ItemsApi/IRepository.cs
+++ src/ItemsApi/IRepository.cs
@@ -1,10 +1,9 @@
-﻿namespace ItemsApi
+﻿namespace ItemsApi;
+
+public interface IRepository<TKey, T>
 {
-    public interface IRepository<TKey, T>
-    {
-        void Create(TKey key, T item);
-        void Delete(TKey key);
-        T? Read(TKey key);
-        void Update(TKey key, T item);
-    }
+    void Create(TKey key, T item);
+    void Delete(TKey key);
+    T? Read(TKey key);
+    void Update(TKey key, T item);
 }
